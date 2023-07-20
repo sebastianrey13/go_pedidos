9e939e8c00590c7757df4d52676258fd22500f87
unknown <areyprada@gmail.com>
2023-07-19 20:09:24 -0500

    se realiza apartadodo de terceros

--- js/nueva-cotizacion.js
+++ js/nueva-cotizacion.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', function () {
 
-//---------------- Obtener Fecha----------------------
+//----------------------------------- Obtener Fecha--------------------------------
 
 function obtenerFechaActual() {
-    var hoy = new Date();
-    var dia = hoy.getDate();
-    var mes = hoy.getMonth() + 1; // Los meses son indexados desde 0, así que sumamos 1
-    var anio = hoy.getFullYear();
+    let hoy = new Date();
+    let dia = hoy.getDate();
+    let mes = hoy.getMonth() + 1; // Los meses son indexados desde 0, así que sumamos 1
+    let anio = hoy.getFullYear();
 
     // Asegurarnos de que el día y el mes tengan siempre dos dígitos (con ceros iniciales si es necesario)
     dia = (dia < 10) ? "0" + dia : dia;
@@ -18,15 +18,94 @@ function obtenerFechaActual() {
 const fechaNuevaCotizacion = document.getElementById('fechaNuevaCotizacion');
 fechaNuevaCotizacion.value = obtenerFechaActual();
 
+//------------------------------------ Terceros ----------------------------------
+
+const imgBuscarTercero = document.querySelector("#imgBuscarTercero");
+const paginaNuevaCotizacion = document.querySelector("#paginaNuevaCotizacion");
+const añadirTerceros = document.querySelector("#añadirTerceros");
+
+imgBuscarTercero.addEventListener("click", function(){
+paginaNuevaCotizacion.classList.add("oculto");
+añadirTerceros.classList.remove("oculto");
+
+let terceros =
+`
+<div class="añadirTerceros">
+    <header>
+        <div class="encabezado">
+            <img id="imgRegresar" src="/img/regresar_30.png" alt="volver atras">
+            <h2>Terceros</h2>
+            <img src="/img/icons8-lupa-30.png" alt="buscarPor">
+        </div>
+    </header>
+
+    <main class="contenedorCotizacines">
+        <div class="cotizacion">
+            <div class="seleccionTercero">
+                <img class="imageAvatar" src="/img/icons8-usuario-de-género-neutro-48.png" alt="">
+                <p id="pRazonSocial1" class="pRazonSocial">UNIVERSIDAD EAFIT</p>
+                <p class="pNit">900000000</p>
+                <img id="imageCheck1" class="imageCheck" src="/img/icons8-casilla-de-verificación-marcada-32.png" alt="">
+            </div>
+            <div class="seleccionTercero">
+                <img class="imageAvatar" src="/img/icons8-usuario-de-género-neutro-48.png" alt="">
+                <p id="pRazonSocial2" class="pRazonSocial">FEDERICO GUTIERREZ</p>
+                <p class="pNit">91500258</p>
+                <img id="imageCheck2" class="imageCheck" src="/img/icons8-casilla-de-verificación-marcada-32.png" alt="">
+            </div>
+            <div class="seleccionTercero">
+                <img class="imageAvatar" src="/img/icons8-usuario-de-género-neutro-48.png" alt="">
+                <p id="pRazonSocial3" class="pRazonSocial">EPM</p>
+                <p class="pNit">800528987</p>
+                <img id="imageCheck3" class="imageCheck" src="/img/icons8-casilla-de-verificación-marcada-32.png" alt="">
+            </div>
+            <div class="seleccionTercero">
+                <img class="imageAvatar" src="/img/icons8-usuario-de-género-neutro-48.png" alt="">
+                <p id="pRazonSocial4" class="pRazonSocial">SURA</p>
+                <p class="pNit">900423154</p>
+                <img id="imageCheck4" class="imageCheck" src="/img/icons8-casilla-de-verificación-marcada-32.png" alt="">
+            </div>
+        </div>
+    </main>
+    </div>
+`;
+añadirTerceros.innerHTML = terceros;
+
+const imgRegresar = document.querySelector("#imgRegresar");
+
+imgRegresar.addEventListener("click", function(){
+    paginaNuevaCotizacion.classList.remove("oculto");
+    añadirTerceros.classList.add("oculto");
+})
+
+// const tercero = document.querySelector("#tercero");
+const tercero = document.querySelector("#tercero");
+
+// Obtener todos los elementos con clase "imageCheck"
+const imageCheckElements = document.querySelectorAll(".imageCheck");
+
+// Agregar un evento de clic a cada elemento "imageCheck"
+imageCheckElements.forEach((element, index) => {
+
+    element.addEventListener('click', () => {
+
+        paginaNuevaCotizacion.classList.remove("oculto");
+        añadirTerceros.classList.add("oculto");
+
+    // Obtener el valor del párrafo con clase "pRazonSocial" correspondiente
+        const pRazonSocialElement = document.getElementById(`pRazonSocial${index + 1}`);
+        const pRazonSocialValue = pRazonSocialElement.textContent;
+        tercero.value = pRazonSocialValue;
+    });
+});
+    
+});
+
+//--------------------------------------
 
 
 
-
-
-
-
-
-
+  
 
 
 
