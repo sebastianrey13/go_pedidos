a0abe32472b221c223f3e123929d9f7c374ee8ab
unknown <areyprada@gmail.com>
2023-07-22 16:32:07 -0500

    se termina apartado de seleccion de producto y se muestra el producto en el apartado de la cotizacion

--- js/nueva-cotizacion.js
+++ js/nueva-cotizacion.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function () {
 
+    // localStorage.clear();
+
 //----------------------------------- Obtener Fecha--------------------------------
 
 function obtenerFechaActual() {
@@ -227,11 +229,7 @@ añadirProducto.addEventListener("click",function(){
     ]
 
     arrayProductos.forEach((producto) => {
-    //   const imagenes = producto.imagenes.join(', ');
-    //   const colores = producto.color.join(', ');
-    //   const valorUnidad = producto.valorUnidad.toLocaleString('es-CO', { style: 'currency', currency: 'COP' });
-    //   const valorDescuento = (producto.valorDescuento).toLocaleString('en-US', { style: 'percent', minimumFractionDigits: 1 });
-    
+   
     let productos =
     `<div class="seleccion">
         <img class="imageAvatar" src="/img/${producto.imgref}" alt="">
@@ -268,7 +266,7 @@ añadirProducto.addEventListener("click",function(){
 
     ////////////////////////////// filtramos Productos //////////////////////////////
 
-    const elementos = document.querySelectorAll(".cotizacion div");
+    const elementos = document.querySelectorAll("#contenerdorProductos div");
 
     function filtrarElementos() {
         const textoBusqueda = inputProductos.value.toLowerCase();
@@ -298,7 +296,7 @@ añadirProducto.addEventListener("click",function(){
 
   const productoSeleccionado = arrayProductos.find(producto => producto.id === idBuscado);
 
-  console.log(productoSeleccionado);
+//   console.log(productoSeleccionado);
 
   if (productoSeleccionado) {
     let añadirDetalleProductos = `
@@ -312,42 +310,49 @@ añadirProducto.addEventListener("click",function(){
 
     <main class="contenedor">
     <div class="productos">
-        <div class="slider">
-            <img src="/img/${productoSeleccionado.imagenes[0]}" alt="tenis_azules"> 
-            ${productoSeleccionado.imagenes.slice(1).map(imagen => `<img class="oculto" src="/img/${imagen}" alt="${imagen.slice(0, -4)}">`).join('\n')}
+        <div id="contenedorPrincipalslider">
+            <div id="imagesSlider" class="slider">
+                <img id="image0" src="/img/${productoSeleccionado.imagenes[0]}" alt="tenis_azules"> 
+                ${productoSeleccionado.imagenes.slice(1).map((imagen, x) => `<img id="image${x+1} "class="oculto" src="/img/${imagen}" alt="${imagen.slice(0, -4)}">`).join('\n')}
+            </div>
             <img class="adelante" src="/img/icons8-más-de-30.png" alt="">
             <img class="atras" src="/img/icons8-menos-de-30.png" alt="">
         </div>
 
         <div class="sliderBoton">
-            <ul class="menu" type="none">
-                ${productoSeleccionado.imagenes.map((imagen, index) => `<li class="boton ${index === 0 ? 'botonRelleno' : ''}"></li>`).join('\n')}
+            <ul class="menuLi" type="none">
+                ${productoSeleccionado.imagenes.map((imagen, index) => `<li id="icono${index+1}" class="boton ${index === 0 ? 'botonRelleno' : ''}"></li>`).join('\n')}
             </ul>
         </div>
 
-        <div>
+        <div id="divColorProducto">
             <label class="colorProducto" for="colorProducto">Color</label>
             <select name="colorProducto" id="colorProducto">
-                ${productoSeleccionado.color.map(color => `<option value="">${color}</option>`).join('\n')}
+                ${productoSeleccionado.color.map((color,x) => `<option id="productoColor${x+1}" value="${x+1}">${color}</option>`).join('\n')}
             </select>
         </div>
 
         <div>
-            <p class="precioConDescuento">$<span>${productoSeleccionado.valorUnidad - productoSeleccionado.valorUnidad * productoSeleccionado.valorDescuento}</span></p>
+            <p class="precioConDescuento">$<span></span></p>
             <div class="precios">
                 <p class="precioNeto">$<span>${productoSeleccionado.valorUnidad}</span></p>
-                <p class="descuento">${productoSeleccionado.valorDescuento * 100}<span>%</span></p>
+                <p class="descuento"><span class="valorDescuento">${productoSeleccionado.valorDescuento * 100}</span><span>%</span></p>
             </div>
         </div>
 
         <div class="cantidad">
-            <img class="mas_menos" src="/img/icons8-menos-30.png" alt="menos">
-            <input class="input__number" type="precio" value="0">
-            <img class="mas_menos" src="/img/icons8-más-30.png" alt="mas">
+            <img id="botonMenos" class="mas_menos" src="/img/icons8-menos-30.png" alt="menos">
+            <input id="inputNumero" class="input_number" type="precio" value="1">
+            <img id="botonMas" class="mas_menos" src="/img/icons8-más-30.png" alt="mas">
+        </div>
+
+        <div class="observacionesProducto">
+            <p>Observaciones del producto</p>
+            <textarea name="" id="textareaObservacionesProducto" cols="30" rows="5"></textarea>
         </div>
 
         <div class="buttonAñadir">
-            <p class="button">Añadir producto</p>
+            <p id="funcionAñadir" class="button">Añadir producto</p>
         </div>
     </div>
     </main>
@@ -356,27 +361,224 @@ añadirProducto.addEventListener("click",function(){
     const detalleProductos = document.querySelector("#detalleProductos");
     detalleProductos.innerHTML = añadirDetalleProductos;
 
-
     paginaNuevaCotizacion.classList.add("oculto");
     añadirProductos.classList.add("oculto");
     detalleProductos.classList.remove("oculto");
 
 
-    const imgRegresarAProductos = document.querySelector("#imgRegresarAProductos");
+    ////////// Regresar menu anterior/////////////////////
 
+    const imgRegresarAProductos = document.querySelector("#imgRegresarAProductos");
     imgRegresarAProductos.addEventListener("click", function(){
         añadirProductos.classList.remove("oculto");
         detalleProductos.classList.add("oculto");
     })
-
-
-
   }
-}
-    
-   ////////////////////////////////////////////////////////////////////////
 
+//////////////////////////// Carrusel de Imagenes  ////////////////////////////////////////////
+
+const atras = document.querySelector(".atras");
+const adelante = document.querySelector(".adelante");
+const imgElement = document.querySelector("#imagesSlider")
+const liEement = document.querySelector(".sliderBoton")
+
+const imagenesCarrusel = imgElement.querySelectorAll(".slider img"); // Selecciona todas las imágenes dentro del slider
+const liRellenos = liEement.querySelectorAll(".menuLi li");
+
+let currentIndex = 0;
+
+adelante.addEventListener("click", ()=>{
+
+    // Oculta la imagen y relleno actual
+    imagenesCarrusel[currentIndex].classList.add('oculto');
+    liRellenos[currentIndex].classList.remove('botonRelleno');
+
+    // Incrementa el índice para mostrar la siguiente imagen y relleno
+    currentIndex++;
+
+    // Si llega al final, vuelve al principio
+    if (currentIndex >= imagenesCarrusel.length) {
+        currentIndex = 0;
+    }
+
+    // Muestra la siguiente imagen
+    imagenesCarrusel[currentIndex].classList.remove('oculto');
+    liRellenos[currentIndex].classList.add('botonRelleno');
+
+    // Actualiza el valor seleccionado en el select de colores
+    document.querySelector("#colorProducto").selectedIndex = currentIndex; 
+
+
+});
+
+atras.addEventListener('click', () => {
+
+    imagenesCarrusel[currentIndex].classList.add('oculto');
+    liRellenos[currentIndex].classList.remove('botonRelleno');
+
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = imagenesCarrusel.length - 1;
+    }
+
+    imagenesCarrusel[currentIndex].classList.remove('oculto');
+    liRellenos[currentIndex].classList.add('botonRelleno');
+
+    document.querySelector("#colorProducto").selectedIndex = currentIndex; 
+
+});
+
+
+////////////////////////// Opciones Manualmente ///////////////////////////////////////
+
+// Agrega un evento 'change' al select para capturar los cambios manuales
+document.querySelector("#colorProducto").addEventListener("change", (event) => {
+    const newIndex = event.target.value - 1; // Restamos 1 para obtener el índice correcto
+    if (newIndex !== currentIndex) {
+        // Oculta la imagen y el liRelleno actual
+        imagenesCarrusel[currentIndex].classList.add('oculto');
+        liRellenos[currentIndex].classList.remove('botonRelleno');
+
+        // Actualiza el índice actual
+        currentIndex = newIndex;
+
+        // Muestra la nueva imagen y el nuevo liRelleno
+        imagenesCarrusel[currentIndex].classList.remove('oculto');
+        liRellenos[currentIndex].classList.add('botonRelleno');
+    }
+});
+
+
+//////////////////Cambio de cantidad de articulos ingresado por el usuario ////////////////////
+
+const botonMenos = document.querySelector("#botonMenos");
+const botonMas = document.querySelector("#botonMas");
+const userInput = document.querySelector("#inputNumero");
+
+let userInputNumber = 1;
+
+botonMas.addEventListener("click", ()=>{
+    userInputNumber++;
+    userInput.value = userInputNumber;
+    console.log(userInputNumber);
+    calcularPrecio();
+});
+
+botonMenos.addEventListener("click", ()=>{
+    userInputNumber--;
+    if(userInputNumber <= 0){
+        userInputNumber = 0;
+    }
+    userInput.value = userInputNumber;
+    console.log(userInputNumber);
+    calcularPrecio();
+});
+
+userInput.addEventListener("input", () => {
+    userInputNumber = parseInt(userInput.value, 10);
+});
+
+//////////////////////////   Valores /////////////////////////////////
+
+
+const precioConDescuento = document.querySelector(".precioConDescuento span");
+const precioNeto = parseFloat(document.querySelector(".precioNeto span").textContent);
+const descuento = parseFloat(document.querySelector(".valorDescuento").textContent);
+
+const porcetajeDescuento = descuento/100;
+
+
+function calcularPrecio(){
+
+    let precioSinDescuento  = precioNeto;
+    let precioTotal = precioSinDescuento - precioSinDescuento*porcetajeDescuento;
+    
+    precioSinDescuento = precioNeto*userInputNumber;
+    precioTotal = precioTotal*userInputNumber;
+
+    precioConDescuento.innerHTML =  precioTotal;
+
+    const valorNeto = document.querySelector(".precioNeto span");
+    valorNeto.textContent = precioSinDescuento;
+}
+
+calcularPrecio()
+
+//////////////////////// Guardar Producto ////////////////////////////////////
+
+const funcionAñadir = document.querySelector("#funcionAñadir");
+
+
+let productoAñadido = {};
+
+funcionAñadir.addEventListener("click", function(){
+
+    // Obtener el tbody de la tabla
+    const tablaProducto = document.querySelector("#tablaProducto");
+    const tbody = document.querySelector("#tablaProducto tbody");
+    tablaProducto.classList.remove("oculto");
+    tbody.innerHTML ="";
+    
+    // localStorage.clear();
+
+    const seleccionColor = document.querySelector("#colorProducto").value;
+    const valorNeto = document.querySelector(".precioNeto span").textContent;
+    const observaciones = document.querySelector("#textareaObservacionesProducto").value;
+
+    const tipoDePedido = document.querySelector("#tipoDePedido").value;
+    const pObservaciones = document.querySelector(".pObservaciones");
+
+    productoAñadido = {
+        "nombre" : document.querySelector(".h2NombreProducto").textContent,
+        "valor unitario" : precioNeto,
+        "cantidad" : userInputNumber,
+        "descuento" : descuento+"%",
+        "Tipo de pedido" : tipoDePedido,
+        "Totales" : parseInt(valorNeto),
+        "Color" : productoSeleccionado.color[seleccionColor - 1]
+    }
+
+
+// Función para agregar una fila a la tabla
+function agregarFila(atributo, valor) {
+    const fila = `
+        <tr>
+            <td>${atributo}</td>
+            <td>${valor}</td>
+        </tr>
+    `;
+    tbody.innerHTML += fila;
+}
+
+// Iterar sobre las propiedades del objeto productoAñadido
+for (const atributo in productoAñadido) {
+    agregarFila(atributo, productoAñadido[atributo]);
+}
+
+pObservaciones.innerHTML = observaciones;
+
+
+
+
+    
+paginaNuevaCotizacion.classList.remove("oculto");
+añadirProductos.classList.add("oculto");
+detalleProductos.classList.add("oculto");
+
+});
+
+
+////////////////////   fin detalle producos /////////////////////////
+}
+
+
+
+////////////////////// fin menu productos//////////////////////
 })
+
+
+
 
   
 })
